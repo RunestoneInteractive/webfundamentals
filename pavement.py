a4101c6066e9c714825f892f82a676a547534662
Brad Miller <bonelake@mac.com>
2015-05-21 14:52:05 +0200

    Move build task definition into the runestone package

--- pavement.py
+++ pavement.py
@@ -4,8 +4,6 @@ import paver.setuputils
 paver.setuputils.install_distutils_tasks()
 import os, sys
 
-from sphinxcontrib import paverutils
-
 sys.path.append(os.getcwd())
 sys.path.append('../modules')
 
@@ -25,6 +23,7 @@ options(
         sourcedir="_sources",
         outdir="./build/"+project_name,
         confdir=".",
+        project_name = project_name,
         template_args = {
             'course_id':project_name,
             'login_required':'false',
@@ -37,45 +36,4 @@ options(
     )
 )
 
-if project_name == "<project_name>":
-  print("Please edit pavement.py and give your project a name")
-  exit()
-
-@task
-@cmdopts([
-    ('all','a','rebuild everything'),
-    ('outputdir=', 'o', 'output static files here'),
-    ('masterurl=', 'u', 'override the default master url'),
-    ('masterapp=', 'p', 'override the default master app')
-])
-def build(options):
-    if 'all' in options.build:
-      options['force_all'] = True
-      options['freshenv'] = True
-
-    if 'outputdir' in options.build:
-        options.build.outdir = options.build.outputdir
-
-    if 'masterurl' in options.build:
-        options.build.template_args['course_url'] = options.build.masterurl
-
-    if 'masterapp' in options.build:
-        options.build.template_args['appname'] = options.build.masterapp
-
-    print('Building into ', options.build.outdir)    
-    rc = paverutils.run_sphinx(options,'build')
-
-
-    try:
-        from chapternames import populateChapterInfo
-        populateChapterInfo(project_name, "%s/index.rst" % options.build.sourcedir)
-        print('Creating Chapter Information')
-    except ImportError:
-        print('Chapter information database population skipped, This is OK for a standalone build.')
-
-    if rc == 0:
-        print("Done, {} build successful".format(project_name))
-    else:
-        print("Error in building {}".format(project_name) )
-
-    
+from runestone import build  # build is called implicitly by the paver driver.
