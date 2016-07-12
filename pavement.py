96aceabedf978c2ae592a49e07df573788244028
Brad Miller <bonelake@mac.com>
2016-07-12 17:43:05 -0500

    update configs

--- pavement.py
+++ pavement.py
@@ -1,18 +1,25 @@
 import paver
 from paver.easy import *
+from socket import gethostname
 import paver.setuputils
 paver.setuputils.install_distutils_tasks()
-import os, sys
-
-sys.path.append(os.getcwd())
-sys.path.append('../modules')
-
+from os import environ
 
 ######## CHANGE THIS ##########
 project_name = "webfundamentals"
 ###############################
 
-master_url = 'http://127.0.0.1:8000'
+# if you want to override the master url do it here.  Otherwise setting it to None
+# configures it for the default case of wanting to use localhost for development
+# and interactivepython for deployment
+
+master_url = None
+if master_url is None:
+    if gethostname() in ['web407.webfaction.com', 'rsbuilder']:
+        master_url = 'http://interactivepython.org'
+    else:
+        master_url = 'http://127.0.0.1:8000'
+
 master_app = 'runestone'
 serving_dir = './build/webfundamentals'
 dest = '../../static'
@@ -34,8 +41,15 @@ options(
             'course_url':master_url,
             'use_services': 'true',
             'python3': 'true',
+            'dburl': 'postgresql://bmiller@localhost/runestone',
+            'basecourse': 'webfundamentals',
         }
     )
 )
+
+# Check to see if we are building on our Jenkins build server, if so use the environment variables
+# to update the DB information for this build
+if 'DBHOST' in environ and  'DBPASS' in environ and 'DBUSER' in environ and 'DBNAME' in environ:
+    options.build.template_args['dburl'] = 'postgresql://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}'.format(**environ)
 
 from runestone import build  # build is called implicitly by the paver driver.
